c44ed91c2db1ea0f6af3db5c4a63c60fbac0903d
mathan-dev-44 <mathang@torus.tech>
2026-04-06 10:50:07 +0530

    updated default theme

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -5,7 +5,7 @@ import type { ThemeStore } from '@/types/theme';
 const useThemeStore = create<ThemeStore>()(
   persist(
     (set) => ({
-      themeKey: 'Ink Blue',
+      themeKey: 'Blossom Pink',
       setTheme: (key: string) => set({ themeKey: key }),
     }),
     { name: 'jf-theme-key' },
